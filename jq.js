db7103408fc5728a7f3a84852985f8f4b201f11a
Shraddha Patel <spatelrb@gmail.com>
2016-03-17 10:38:49 -0400

    fixed things

--- jq.js
+++ jq.js
@@ -1,11 +1,48 @@
 $(document).ready(function(){
 
     $("h1").click(function(){
-        $(this).hide();
+      $(this).hide();
     });
 
-    $("a").click(function(){
-
+    $(".puppy").hover(function(){
+    	$(".puppy").fadeOut(1000);
     });
+
+    $("#two").click(function(){
+    	$(".black").animate({height: "400px"})
+    });
+
+    $("#three").click(function(){
+    	$(".pink").animate({opacity: "0.5"});
+    });
+
+    $("#four").click(function(){
+    	$(".green").animate({height: "toggle"});
+    });
+
+    $("#five").click(function(){
+    	$(".red").slideUp(2000);
+    });
+
+    $("#hide").click(function(){
+        $("p").hide();
+    });
+    $("#show").click(function(){
+        $("p").show();
+    });
+
+    $("#seven").click(function(){
+    	$(".yellow").slideUp(2000)
+    							.slideDown(2000);
+    });
+
+    $("#hover").hover(function(){
+    	$("#pull").slideDown("slow");
+    })
+
+    $("#ten").click(function(){
+    $(".orange").animate({width: "100px"});
+    });
+
 
 });
---
+++ jqcheatsheet.md
@@ -0,0 +1,9 @@
+A library is a prewritten collection of functions and code to make a set of tasks easier in a specific programming language. 
+You have to include library in your html code to have access to its functions and methods. Be careful because some libraries can conflict with each other and you may need to remove a library or edit it to get them to work together. 
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
+</script> --> google content delivery network that should be included in the head of your html
+
+jQuery can be used to select elements using a similar syntax to CSS. You can call a class or id. Eg. $("element name, class, id")
+
+
